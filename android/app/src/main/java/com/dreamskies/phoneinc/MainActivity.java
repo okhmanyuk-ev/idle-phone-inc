a1df895148d09c627f2fec77f512365e68446165
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2020-04-20 15:52:03 +1000

    upd android

--- android/app/src/main/java/com/dreamskies/phoneinc/MainActivity.java
+++ android/app/src/main/java/com/dreamskies/phoneinc/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import com.dreamskies.sky.SkyActivity;
 
 public class MainActivity extends SkyActivity {
+    static {
+        System.loadLibrary("phoneinc");
+    }
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
