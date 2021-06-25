4fdcf3a7b27c595626a9b729ce143b67d0b43c89
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2021-06-25 19:46:10 +1000

    upd

--- src/screens/screen.h
+++ src/screens/screen.h
@@ -4,28 +4,5 @@
 
 namespace PhoneInc
 {
-	class Screen : public Shared::SceneManager::Screen, public std::enable_shared_from_this<Screen>
-	{
-	public:
-		Screen();
-
-	public:
-		auto getContent() const { return mContent; }
-
-	protected:
-		void onEnterBegin() override;
-		void onEnterEnd() override;
-		void onLeaveBegin() override;
-		void onLeaveEnd() override;
-		void onWindowAppearing() override;
-		void onWindowDisappearing() override;
-
-	protected:
-		std::unique_ptr<Actions::Action> createEnterAction() override;
-		std::unique_ptr<Actions::Action> createLeaveAction() override;
-
-	private:
-		std::shared_ptr<Scene::Node> mContent;
-		std::shared_ptr<Scene::Rectangle> mFadeRectangle;
-	};
+	using Screen = Shared::SceneHelpers::StandardScreen;
 }
--- src/windows/window.h
+++ src/windows/window.h
@@ -5,29 +5,7 @@
 
 namespace PhoneInc
 {
-	class Window : public Scene::Clickable<Shared::SceneHelpers::Backshaded<Shared::SceneManager::Window>>
-	{
-	public:
-		Window();
-		
-	public:
-		void onOpenEnd() override;
-		void onCloseBegin() override;
-
-	public:
-		std::unique_ptr<Actions::Action> createOpenAction() override;
-		std::unique_ptr<Actions::Action> createCloseAction() override;
-
-	public:
-		auto getContent() { return mContent; }
-
-		auto getCloseOnMissclick() const { return mCloseOnMissclick; }
-		void setCloseOnMissclick(bool value) { mCloseOnMissclick = value; }
-
-	private:
-		std::shared_ptr<Scene::Node> mContent;
-		bool mCloseOnMissclick = true;
-	};
+	using Window = Shared::SceneHelpers::StandardWindow;
 
 	class StandardWindow : public Window
 	{
