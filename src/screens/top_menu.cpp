4ee67c349131dab7928687fde210223cf600ea74
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2025-10-09 17:25:25 +1000

    move top_menu xml script to assets/xml/top_menu.xml

--- src/screens/top_menu.cpp
+++ src/screens/top_menu.cpp
@@ -8,21 +8,9 @@ TopMenu::TopMenu()
 	setTouchable(true);
 	setVerticalOrigin(168.0f);
 
-	auto [node, collection] = Shared::SceneHelpers::CreateNodesFromXml(R"(
-		<Sprite texture="textures/top_menu/background.png" batch_group="ui_menu">
-			<Sprite anchor="0,1" pivot="0.5" pos="454,-56" texture="textures/top_menu/money_bg.png" batch_group="ui_menu_background">
-				<MyLabelSolid id="cash_label" anchor="1,0.5" pivot="1,0.5" x="-64" font_size="39" color="rgb(0,255,41) "/>
-				<Sprite anchor="0,0.5" pivot="0.5" texture="textures/top_menu/money.png" batch_group="ui_menu_icon"/>
-				<MyButton anchor="1,0.5" pivot="0.5" texture="textures/top_menu/add.png" batch_group="ui_menu_icon"/>
-			</Sprite>
-			<Sprite anchor="0,1" pivot="0.5" pos="900,-56" texture="textures/top_menu/coin_bg.png" batch_group="ui_menu_background">
-				<MyLabelSolid id="coins_label" anchor="1,0.5" pivot="1,0.5" x="-64" font_size="39" color="rgb(255,192,24) "/>
-				<Sprite anchor="0,0.5" pivot="0.5" texture="textures/top_menu/coin.png" batch_group="ui_menu_icon"/>
-				<MyButton anchor="1,0.5" pivot="0.5" texture="textures/top_menu/add.png" batch_group="ui_menu_icon"/>
-			</Sprite>
-			<MyButton anchor="0,1" pivot="0.5" pos="67,-56" texture="textures/top_menu/settings.png" batch_group="ui_menu_icon"/>
-		</Sprite>
-	)");
+	auto asset = sky::Asset("xml/top_menu.xml");
+	auto xml = std::string((const char*)asset.getMemory(), asset.getSize());
+	auto [node, collection] = Shared::SceneHelpers::CreateNodesFromXml(xml);
 	attach(node);
 
 	auto cash_label = std::static_pointer_cast<Helpers::LabelSolid>(collection.at("cash_label"));
