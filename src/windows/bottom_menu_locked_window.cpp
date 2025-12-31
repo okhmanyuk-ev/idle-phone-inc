550efd6a0b3a49712bfa97f28eb4a4dbe474257f
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2025-12-31 15:36:48 +1000

    upd sky

--- src/windows/bottom_menu_locked_window.cpp
+++ src/windows/bottom_menu_locked_window.cpp
@@ -21,7 +21,7 @@ BottomMenuLockedWindow::BottomMenuLockedWindow()
 	description->setPosition({ 0.0f, -182.0f });
 	description->setFontSize(52.0f);
 	description->setMultiline(true);
-	description->setAlign(Graphics::TextMesh::Align::Center);
+	description->setAlign(sky::TextMesh::Align::Center);
 	description->setWidth(546.0f);
 	bg->attach(description);
 }
